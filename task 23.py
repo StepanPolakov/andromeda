aa0851fae84483ee9c2952afdb0dee24a01a9a8f
StepanPolakov <107405016+StepanPolakov@users.noreply.github.com>
2022-06-13 14:26:48 +0300

    Add files via upload

---
+++ task 23.py
@@ -0,0 +1,27 @@
+number = '132'
+print(str(number))
+print(int(number))
+print(bool(number))
+print(list(number))
+money=69420
+money=money-40000    
+print(money)
+money=money-5136.8
+print(money)
+money=money-400*30
+print(money)
+print(type(money))
+print(money>10000)
+print(money<10000)
+(int('5')>=5)
+vanya=12
+mother=28+12
+sister=mother/2
+father=sister*2.4
+print(father)
+glass=15
+day=glass*5
+month=28
+how_many_weeks=month/7
+work_days=how_many_weeks*5
+print(work_days*day)
