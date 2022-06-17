2653bf6c1dc475607e7544243dcab0894c044866
StepanPolakov <107405016+StepanPolakov@users.noreply.github.com>
2022-06-17 14:04:21 +0300

    Add files via upload

--- fanct.py
+++ fanct.py
@@ -1,15 +1,32 @@
-num1=input('Введите первое число:')
-num1=int(num1)
-opperator=input('Введите знак:')
-num2=input('Введите второе число:')
-num2=int(num2)
-def plus(a,b):
-    return a+b
-
-if opperator =='+':
-    answer=plus(num1,num2)
-    print(f'Ответ:{answer}')
-
+while True:
+    num1=input('Введите первое число:')
+    num1=int(num1)
+    opperator=input('Введите знак:')
+    num2=input('Введите второе число:')
+    num2=int(num2)
+    def plus(a,b):
+        return a+b
+    def minus(a,b):
+        return a-b
+    def multipl(a,b):
+        return a*b
+    def divide(a,b):
+        return a/b
+    if opperator =='+':
+        answer=plus(num1,num2)
+        print(f'Ответ:{answer}')
+    if opperator =='-':
+        answer=minus(num1,num2)
+        print(f'Ответ:{answer}')
+    if opperator =='*':
+        answer=multipl(num1,num2)
+        print(f'Ответ:{answer}')
+    if opperator =='/':
+        answer=divide(num1,num2)
+        print(f'Ответ:{answer}')
+    stop=input('Закончить?')
+    if stop=='да':
+        break
 
 
 
